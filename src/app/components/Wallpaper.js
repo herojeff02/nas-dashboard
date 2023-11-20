0702e9509b6fcaa890698c4d4511d046587f329c
Junoh Cho <herojeff02@gmail.com>
2023-11-20 23:31:02 +0900

    feat: Altered Wallpaper to use Unsplash images

--- src/app/components/Wallpaper.js
+++ src/app/components/Wallpaper.js
@@ -1,10 +1,17 @@
 import React, {useEffect, useState} from "react";
 
-export default function Wallpaper() {
+export default function Wallpaper({setLoaded = (a) => {}}) {
     const [wallpaperPath, setWallpaperPath] = useState("")
     const [curtain, setCurtain] = useState(true)
 
-    const revealCurtain = () => setTimeout(() => setCurtain(false), 200)
+    const revealCurtain = () => {
+        setTimeout(() => {
+            setCurtain(false)
+        }, 200)
+        setTimeout(() => {
+            setLoaded(true)
+        }, 450)
+    }
 
     useEffect(() => {
         setWallpaper()
@@ -16,63 +23,40 @@ export default function Wallpaper() {
             setTimeout(() => {
                 setWallpaper()
             }, 4000)
-        }, 5400000)
+        }, 1000*60*30)
+
+        setTimeout(()=>{revealCurtain()}, 5000)
     }, []);
 
 
     function setWallpaper() {
         let wall_path = [
-            "/wallpaper/img100.jpg",
-            "/wallpaper/img101.png",
-            "/wallpaper/img102.jpg",
-            "/wallpaper/img103.png",
-            "/wallpaper/img104.jpg",
-            "/wallpaper/img7.jpg",
-            "/wallpaper/img8.jpg",
-            "/wallpaper/img10.jpg",
-            "/wallpaper/img11.jpg",
-            "/wallpaper/img12.jpg",
-            "/wallpaper/img1.jpg",
-            "/wallpaper/img2.jpg",
-            "/wallpaper/img3.jpg",
-            "/wallpaper/img4.jpg",
-            "/wallpaper/holo/Background_Cliffhouse.jpg",
-            "/wallpaper/holo/Background_Safety_Objects.jpg",
-            "/wallpaper/holo/bg1a.jpg",
-            "/wallpaper/holo/bg2.jpg",
-            "/wallpaper/holo/bg3.jpg",
-            "/wallpaper/holo/bg4.jpg",
-            "/wallpaper/holo/bg5.jpg",
-            "/wallpaper/holo/bg6.jpg",
-            "/wallpaper/holo/bg7.jpg",
-            "/wallpaper/holo/FR_Back_Landscape_Med_1920x1080.jpg",
-            "/wallpaper/weather/farewell.jpg",
-            "/wallpaper/weather/fre_background.jpg",
+            "abduzeedo",
+            "heysupersimi",
+            "oostarteroo",
+            "eberhardgross",
+            "limamauro23"
         ];
-        let wall = Math.random() * wall_path.length;
-        wall -= wall % 1;
-        let final_wall = wall_path[wall];
-
-        setWallpaperPath(final_wall)
-
+        setWallpaperPath("https://source.unsplash.com/user/"+wall_path[Math.floor(Math.random() * wall_path.length)]+"/"+window.innerWidth+"x"+window.innerHeight)
     }
 
     return (
-        <div style={{width: "100%", height: "100%"}}>
+        <div style={{width: "100%", height: "100%", position: "absolute", left: 0, top: 0}}>
             <img onLoad={revealCurtain} src={wallpaperPath} alt={""} width={"100%"}
                  height={"100%"}
                  style={{
                      objectFit: "cover",
-                     opacity: 0.6,
+                     opacity: 0.8,
                  }}/>
             <div style={{
                 background: "black",
                 position: "absolute",
-                right: 0,
+                left: 0,
                 top: 0,
                 height: "100%",
-                width: curtain ? "100%" : "0%",
-                transition: "width 0.6s ease"
+                width: "100%",
+                transform: curtain ? "translate3d(0%,0,0)" : "translate3d(100%,0,0)",
+                transition: "transform 0.6s ease"
             }}/>
         </div>
     )
