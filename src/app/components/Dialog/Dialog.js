80cdaf7f19871d78595b099e88754452a54bee3a
Junoh Cho <herojeff02@gmail.com>
2023-11-22 02:02:19 +0900

    feat: Altered Dialog design

--- src/app/components/Dialog/Dialog.js
+++ src/app/components/Dialog/Dialog.js
@@ -3,15 +3,16 @@ import React from "react";
 import ErrorIcon from "../../Icons/ErrorIcon";
 import ApprovedIcon from "../../Icons/ApprovedIcon";
 import QuestionIcon from "../../Icons/QuestionIcon";
-import CenterSheet from "../Sheet/CenterSheet";
+import BottomSheet from "../Sheet/BottomSheet";
 import PressableElement from "../PressableElement";
 import AlignCenterContainer from "../AlignCenterContainer";
+import Loading from "react-loading";
 
 const Dialog = (
     {
         mainText = "",
-        primaryButtonText = "확인",
-        primaryButtonTextColor = "#fff",
+        primaryButtonText = undefined,
+        primaryButtonTextColor = "#000",
         secondaryButtonText = undefined,
         isVisible = false,
         setVisible = _ => {
@@ -25,93 +26,100 @@ const Dialog = (
         showSuccessIcon = false,
         showFailIcon = false,
         showQuestionIcon = false,
+        showLoadingIcon = false,
     }) => {
 
     return (<>
-        <CenterSheet
+        <BottomSheet
             isDismissible={false}
             isOpen={isVisible}
             setOpen={setVisible}
+            preferredWidth={"400px"}
         >
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
-                position: "relative"
+                position: "relative",
+                alignItems:"center"
             }}>
-                <div style={{ textAlign: "center", margin: "12px 0 0 0" }}>
+                <div style={{textAlign: "center", margin: "30px 0 12px 0", height:"34px", width:"34px"}}>
                     {showSuccessIcon &&
-                        <ApprovedIcon width={34} height={34} fill={"#3b9600"}
-                                      style={{ margin: "2px 0 3px 0" }} />}
+                        <ApprovedIcon width={34} height={34} fill={"#3b9600"}/>}
                     {showFailIcon &&
-                        <ErrorIcon width={34} height={34} fill={"#af0000"} style={{ margin: "2px 0 3px 0" }} />}
+                        <ErrorIcon width={34} height={34} fill={"#af0000"}/>}
                     {showQuestionIcon &&
-                        <QuestionIcon width={34} height={34} fill={"#f95700"} style={{ margin: "2px 0 3px 0" }} />}
+                        <QuestionIcon width={34} height={34} fill={"#f95700"}/>}
+                    {showLoadingIcon &&
+                        <Loading type={"spin"} width={34} height={34} color={"#af0000"}/>}
                 </div>
                 <ModalMainText>
                     {mainText}
                 </ModalMainText>
-                <div style={{ display: "flex", alignItems: "stretch", borderTop: `1px solid #22222250` }}>
+                <div style={{display: "flex", alignItems: "stretch", borderTop: `1px solid #22222210`, width:"100%"}}>
                     {!!secondaryButtonText &&
                         <>
                             <PressableElement
-                                $borderEnabled={false}
-                                $scaleEnabled={false}
+                                borderEnabled={false}
+                                scaleEnabled={false}
                                 height={null}
                                 onClick={() => {
                                     secondaryButtonAction();
                                     finalAction()
                                 }}>
                                 <AlignCenterContainer height={"100%"}>
-                                    <ModalButtonText text={secondaryButtonText} style={{ color: "#fff" }}>
+                                    <ModalButtonText text={secondaryButtonText} style={{color: "#000"}}>
                                         {secondaryButtonText}
                                     </ModalButtonText>
                                 </AlignCenterContainer>
                             </PressableElement>
-                            <div style={{borderRight: `1px solid #22222250`}}/>
+                            <div style={{borderRight: `1px solid #22222210`}}/>
                         </>
                     }
-                    <PressableElement
-                        $borderEnabled={false}
-                        $scaleEnabled={false}
-                        height={null}
-                        onClick={() => {
-                            primaryButtonAction();
-                            finalAction()
-                        }}>
-                        <AlignCenterContainer height={"100%"}>
-                            <ModalButtonText text={primaryButtonText} style={{ color: primaryButtonTextColor }}>
-                                {primaryButtonText}
-                            </ModalButtonText>
-                        </AlignCenterContainer>
-                    </PressableElement>
+                    {!!primaryButtonText &&
+                        <PressableElement
+                            borderEnabled={false}
+                            scaleEnabled={false}
+                            clickColor={"#EEE"}
+                            height={null}
+                            onClick={() => {
+                                primaryButtonAction();
+                                finalAction()
+                            }}>
+                            <AlignCenterContainer height={"100%"}>
+                                <ModalButtonText text={primaryButtonText} style={{color: primaryButtonTextColor}}>
+                                    {primaryButtonText}
+                                </ModalButtonText>
+                            </AlignCenterContainer>
+                        </PressableElement>
+                    }
                 </div>
             </div>
-        </CenterSheet>
+        </BottomSheet>
     </>);
 };
 
 const ModalButtonText = styled.p`
-    font-weight: 600;
-    font-size: 14px;
-    margin: 6px;
-    text-align: center;
-    padding: 10px 0;
+  font-weight: 600;
+  font-size: 14px;
+  margin: 6px;
+  text-align: center;
+  padding: 10px 0;
 
-    ${({ text }) => text === "삭제" ? "color: #FF5757" : null}
-    ${({ disabled, selected }) => (disabled && !selected) ? `background: #D9D9D9` : null}
+  ${({text}) => text === "삭제" ? "color: #FF5757" : null}
+  ${({disabled, selected}) => (disabled && !selected) ? `background: #D9D9D9` : null}
 `;
 const ModalMainText = styled.p`
-    padding: 8px 20px 20px 20px;
-    font-weight: 500;
-    font-size: 16px;
-    margin: 0;
-    text-align: center;
-    color: #fff;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  padding: 5px 20px 35px 20px;
+  font-weight: 500;
+  font-size: 16px;
+  margin: 0;
+  text-align: center;
+  color: #000;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Dialog;
