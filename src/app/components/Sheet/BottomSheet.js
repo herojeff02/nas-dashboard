d8776bbe972b46079f2b1665e22f4fabfa157143
Junoh Cho <herojeff02@gmail.com>
2023-11-21 22:30:21 +0900

    feat: Altered Sheet design

--- src/app/components/Sheet/BottomSheet.js
+++ src/app/components/Sheet/BottomSheet.js
@@ -1,19 +1,22 @@
 import styled from "styled-components";
-import { useEffect, useRef, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 
-export default function CenterSheet (
+export default function BottomSheet(
     {
         children,
         isDismissible = true,
         isOpen = true,
-        setOpen = alert,
+        setOpen = (a) => {
+        },
         preferredHeight = "auto" || 100,
+        preferredWidth = "auto" || 100,
         hideOverflow = true,
     }) {
 
     const [isVisible, setVisible] = useState(false);
     const [sheetHeight, setSheetHeight] = useState(0);
+    const [sheetWidth, setSheetWidth] = useState(0);
 
     const sheetRef = useRef()
     const childRef = useRef()
@@ -21,13 +24,13 @@ export default function CenterSheet (
 
 
     const animateClose = () => {
-        if(isDismissible && !!setOpen) {
+        if (isDismissible && !!setOpen) {
             setOpen(false)
         }
     }
 
     useEffect(() => {
-        if(isOpen) {
+        if (isOpen) {
             setVisible(true);
         } else {
             setTimeout(() => {
@@ -36,13 +39,19 @@ export default function CenterSheet (
         }
 
         const resizeObserver = new ResizeObserver(() => {
-            if(preferredHeight === "auto") {
+            if (preferredHeight === "auto") {
                 setSheetHeight(childRef?.current?.offsetHeight)
             } else {
                 setSheetHeight(preferredHeight);
             }
+
+            if (preferredWidth === "auto") {
+                setSheetHeight(childRef?.current?.offsetWidth)
+            } else {
+                setSheetWidth(preferredWidth);
+            }
         });
-        if(childRef.current) {
+        if (childRef.current) {
             resizeObserver.observe(childRef.current);
         }
         return () => resizeObserver.disconnect();
@@ -54,39 +63,41 @@ export default function CenterSheet (
             transition: "background 0.3s, opacity 0.3s",
             pointerEvents: isOpen ? `all` : `none`,
             opacity: isVisible ? `1` : `0`,
-            background: `rgba(0, 0, 0, ${isOpen ? 0.2 : 0})`
+            background: `rgba(0, 0, 0, ${isOpen ? 0.5 : 0})`
         }}>
             <div
-                style={{ width: "100%", height: "100%" }}
+                style={{width: "100%", height: "100%"}}
                 onClick={(e) => {
                     animateClose()
-                }} />
+                }}/>
 
             <Sheet
                 onClick={(e) => {
-                    if(isOpen) {
+                    if (isOpen) {
                         e.stopPropagation()
                     }
                 }}
                 ref={sheetRef}
                 style={{
                     height: sheetHeight,
+                    width: sheetWidth,
                     overflow: hideOverflow ? "hidden" : "default",
                     pointerEvents: isOpen ? `all` : `none`,
                     transition: `opacity 0.25s, transform 0.3s`,
                     opacity: isOpen ? `1` : `0`,
-                    transform: isOpen ? `scale3d(1.0, 1.0, 1)` : `scale3d(0.95, 0.95, 1)`
+                    transform: isOpen ? `translate3d(-50%,0,0)` : `translate3d(-50%, 10px, 0)`
                 }}
             >
                 <div
-                    ref={scrollRef} style={{
-                    position: "absolute",
-                    height: "100%",
-                    width: "100%",
-                    overflow: "scroll",
-                    pointerEvents: isOpen ? `all` : `none`,
-                    overscrollBehavior: "none"
-                }}>
+                    ref={scrollRef}
+                    style={{
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%",
+                        overflow: "scroll",
+                        pointerEvents: isOpen ? `all` : `none`,
+                        overscrollBehavior: "none"
+                    }}>
                     <div ref={childRef}>{children}</div>
                 </div>
             </Sheet>
@@ -95,30 +106,26 @@ export default function CenterSheet (
 
 
 const Background = styled.div`
-    position: fixed;
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    z-index: 9040;
-  
-  left: 50%;
-  transform: translate3d(-50%, 0, 0);
+  position: fixed;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  z-index: 9040;
 `
 
 const Sheet = styled.div`
-    width: calc(100% - 20px);
-    max-height: calc(100% - 20px);
-    position: absolute;
-    border-radius: 10px;
-    box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
-    background: rgba(0,0,0,0.6);
-  backdrop-filter: blur(20px);
-    z-index: 9041;
-    max-width: 500px;
-    margin: auto;
+  width: 100%;
+  left: 50%;
+  bottom: 20px;
+  max-height: calc(100% - 20px);
+  position: absolute;
+  border-radius: 8px;
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
+  background: white;
+  z-index: 9041;
+  margin: auto;
 
 `
